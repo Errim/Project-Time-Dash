b47d21af5eb53b4bcea30b5bd76a47da914cd2ff
Emil Jacobsen <ppoy52@gmail.com>
2014-07-19 14:19:12 +0200

    Fixed some mistakes in Env code

--- Java/src/com/ludumdare/game/Environment.java
+++ Java/src/com/ludumdare/game/Environment.java
@@ -12,14 +12,11 @@ public class Environment {
 			tile_width = 32, tile_height = 32; /* 30x20 */
 	public int tiles[] = new int[num_wide * num_high];
 
-	public int get_tile(int xth, int yth) {
-		return (num_high * xth) + yth;
+	public boolean tile_clear(int x, int y) {
+		return (tiles[find_tile(x, y)] == 0);
 	}
-	public int get_xth(int tile) {
-		return tile / num_high;
-	}
-	public int get_yth(int tile) {
-		return tile % num_high;
+	public int find_tile(float x, float y) {
+		return get_tile(x / tile_width, y / tile_height);
 	}
 	public int get_x(int tile) {
 		return get_xth(tile) * tile_width;
@@ -27,16 +24,20 @@ public class Environment {
 	public int get_y(int tile) {
 		return get_yth(tile) * tile_height;
 	}
-	public int find_tile(float x, float y) {
-		return get_tile(Dash_component.GAME_W % tile_width, Dash_component.GAME_H % tile_width);
-	}
-	public boolean tile_clear(int x, int y) {
-		return (tiles[find_tile(x, y)] == 0);
-	}
 	public void draw(Graphics g) {
 		for (int i = 0 ; i < tiles.length ; i++) {
 			if (tiles[i] != 0) { draw_tile(g, i); }
 		}
+	}
+	/* Private */
+	private int get_tile(int xth, int yth) {
+		return (num_high * xth) + yth;
+	}
+	private int get_xth(int tile) {
+		return tile / num_high;
+	}
+	private int get_yth(int tile) {
+		return tile % num_high;
 	}
 	private void draw_tile(Graphics g, int tile) {
 
