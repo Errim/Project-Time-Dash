0cd1cd256e0f468b92c617e07d0742bee66247c6
Emil Jacobsen <ppoy52@gmail.com>
2014-07-19 12:38:47 +0200

    Spelling error fixed

--- Java/src/com/ludumdare/game/Environment.java
+++ Java/src/com/ludumdare/game/Environment.java
@@ -4,7 +4,7 @@ package com.ludumdare.game;
  * Created by J on 19/07/2014.
  */
 
-public class Environement {
+public class Environment {
 	private int num_wide = 30, num_high = 20; /* 30x20 */
 	public int tiles[] = new int[num_wide * num_high];
 
