90ad86baea0e766f4bae9ddcd12171c6bfee3fa3
Emil Ström <emil.strm@gmail.com>
2014-07-20 20:17:34 +0200

    I dont even know

--- Java/src/com/ludumdare/game/Game.java
+++ Java/src/com/ludumdare/game/Game.java
@@ -58,18 +58,21 @@ public class Game {
 	}
 
 	public void spawn_enemy() {
+		float spawn_x, spawn_y;
+
+		do {
+			spawn_x = (float)GameMath.getRndDouble(0, environment.num_wide * environment.tile_width);
+			spawn_y = (float)GameMath.getRndDouble(0, environment.num_high * environment.tile_height);
+		} while(environment.collision(spawn_x, spawn_y, 13, 13) ||
+				(spawn_x >= game_screen.get_x() && spawn_x < game_screen.get_x() + game_screen.width &&
+				spawn_y >= game_screen.get_y() && spawn_y < game_screen.get_y() + game_screen.height));
+
 		int enemy_type =  GameMath.getRndInt(1, 4);
 		if (enemy_type < 4) { /* Flyer */
-			enemy_list[enemy_index] = new Flyer(GameMath.getRndInt(0, environment.num_wide * environment.tile_width), GameMath.getRndInt(0, environment.num_high * environment.tile_height), 12, 8, Actor.face.LEFT, this);
+			enemy_list[enemy_index] = new Flyer(spawn_x, spawn_y, 12, 8, Actor.face.LEFT, this);
 		} else if (enemy_type == 4) { /* Bouncer */
-			int w = 13, h = 13,
-					x = GameMath.getRndInt(0, environment.num_high * environment.tile_height),
-					y = GameMath.getRndInt(0, environment.num_wide * environment.tile_width);
-			while (environment.collision(x, y, w, h)) {
-				x = GameMath.getRndInt(0, environment.num_high * environment.tile_height);
-				y = GameMath.getRndInt(0, environment.num_wide * environment.tile_width);
-			}
-		enemy_list[enemy_index] = new Bouncer(x, y, w, h, Actor.face.LEFT, this);
+			int w = 13, h = 13;
+			enemy_list[enemy_index] = new Bouncer(spawn_x, spawn_y, w, h, Actor.face.LEFT, this);
 		}
 		enemy_index = (enemy_index + 1) % enemy_list.length;
 	}
